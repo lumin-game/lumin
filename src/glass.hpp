974f3abe0f7983cfb1900784844f37350281272d
Sherry Yuan <frostyyshadows@gmail.com>
2019-02-10 21:04:11 -0800

    changed texture path

--- src/glass.hpp
+++ src/glass.hpp
@@ -7,8 +7,8 @@ class Glass : public Entity {
 public:
 	~Glass() override { Entity::destroy(); }
 
-	const char* get_texture_path() override { return textures_path("wall.png"); }
+	const char* get_texture_path() override { return textures_path("glass.png"); }
 	bool is_player_collidable() const override { return true; }
 	bool is_light_collidable() const override { return false; }
-	EntityColor get_color() const override { return EntityColor({0.7, 0.7, 1.0, 0.8}); }
+	EntityColor get_color() const override { return EntityColor({1.0, 1.0, 1.0, 1.0}); }
 };
