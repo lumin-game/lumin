3439f98c72841a5396fb7ad6594e4081c6f4a3f9
Soohyun Kim <shkim7534@gmail.com>
2019-03-23 22:54:51 -0700

    top menu adding
    top menu scaled
    
    replaced with transparent background
    
    added current level top menu textures
    
    current level top menu added
    
    renaming files + textures
    
    refactoring
    
    current level showing on each screen
    
    added menu to right top menu

--- src/game_screen.hpp
+++ src/game_screen.hpp
@@ -8,17 +8,20 @@ public:
 	virtual ~GameScreen() { GameScreen::destroy(); };
 
 	virtual const char* get_texture_path() const = 0;
-	// Creates all the associated render reso+urces and default transform
-	virtual bool init();
+	// Creates all the associated render resources and default transform
+	virtual bool init(vec2 screen);
 
 	// Releases all the associated resources
 	virtual void destroy();
 
 	void draw(const mat3& projection)override;
 
-	void set_position(vec2 position) {
-		m_position = position;
+	virtual void set_position(vec2 position, vec2 offset = { 0, 0 }) {
+		m_position = position + offset;
 	}
+
+	virtual const vec2 set_translation_scale() { return { 1.f, 1.f }; };
+	virtual const vec2 set_translation_position() { return { 0, 0 }; };
 
 	vec2 get_bounding_box()const;
 
