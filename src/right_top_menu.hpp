8afc0717a6cc04477752d006d22ddc2d3d55c02f
Soohyun Kim <shkim7534@gmail.com>
2019-04-22 21:19:44 -0700

    refactoring + scaling

--- src/right_top_menu.hpp
+++ src/right_top_menu.hpp
@@ -6,7 +6,7 @@ class RightTopMenu : public GameScreen
 {
 public:
 	const char* get_texture_path() const override { return textures_path("right_top_menu.png"); }
-	void set_translation_scale() override { m_scale = { 0.12, 0.12 }; };
+	void set_translation_scale() override { m_scale = { 0.15, 0.15 }; };
 	void set_translation_position(vec2 screen, bool is_true) override { 
 		is_true = false; 
 		m_position = calculate_position(screen, is_true); 
